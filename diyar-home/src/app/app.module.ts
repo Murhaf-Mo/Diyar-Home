7f6836f649bf2181b0c187ea3b6a24a5b87bde39
Murhaf-Mo <99115205+Murhaf-Mo@users.noreply.github.com>
2023-07-07 19:07:32 +0300

    start of home

--- diyar-home/src/app/app.module.ts
+++ diyar-home/src/app/app.module.ts
@@ -4,22 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NbThemeModule, NbLayoutModule, NbCardModule, NbTabsetModule} from '@nebular/theme';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbCardModule,
+  NbTabsetModule,
+  NbActionsModule,
+  NbIconModule
+} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbThemeModule.forRoot({name: 'corporate'}),
     NbLayoutModule,
     NbEvaIconsModule,
     NbCardModule,
     NbTabsetModule,
+    NbActionsModule,
+    NbIconModule,
 
   ],
   providers: [],
