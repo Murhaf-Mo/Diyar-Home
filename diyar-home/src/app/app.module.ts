7454dc45d05c78b44465ce1ea64c8e62f518a978
Murhaf-Mo <99115205+Murhaf-Mo@users.noreply.github.com>
2023-07-07 23:52:59 +0300

    numbersss!!!!!!!!!

--- diyar-home/src/app/app.module.ts
+++ diyar-home/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,6 +27,7 @@ import { HomeComponent } from './home/home.component';
     NbLayoutModule,
     NbEvaIconsModule,
     NbCardModule,
+    NbIconModule,
     NbTabsetModule,
     NbActionsModule,
     NbIconModule,
