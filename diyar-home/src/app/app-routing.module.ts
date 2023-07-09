6a70cc9b6211b8713246a2ec6adad9ae2f95345b
Murhaf-Mo <99115205+Murhaf-Mo@users.noreply.github.com>
2023-07-09 08:40:45 +0300

    Update app-routing.module.ts

--- diyar-home/src/app/app-routing.module.ts
+++ diyar-home/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'home', component: AppComponent }
+  { path: 'home', component: AppComponent },
+  { path: '', component: AppComponent }
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
